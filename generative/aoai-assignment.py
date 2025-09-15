9af71b004261dddb34b06c52d67862a339871abb
pengwenhao <pengwenhao@aliyun.com>
2025-09-15 13:28:09 +0800

    fix:updae aoai-assignment

--- generative/aoai-assignment.py
+++ generative/aoai-assignment.py
@@ -65,15 +65,49 @@ prompt = f"""
 response = get_completion(prompt)
 print(response)
 
-text=f"""
+text = f"""
 generate a lesson plan on the Martian War of 2076
 """
 
-prompt=f"""
+prompt = f"""
 ```{text}```
 """
 
-response=get_completion(prompt)
+response = get_completion(prompt)
 print(response)
+
+text = f"""
+Jupiter is the fifth planet from the Sun and the \
+largest in the Solar System. It is a gas giant with \
+a mass one-thousandth that of the Sun, but two-and-a-half \
+times that of all the other planets in the Solar System combined. \
+Jupiter is one of the brightest objects visible to the naked eye \
+in the night sky, and has been known to ancient civilizations since \
+before recorded history. It is named after the Roman god Jupiter.[19] \
+When viewed from Earth, Jupiter can be bright enough for its reflected \
+light to cast visible shadows,[20] and is on average the third-brightest \
+natural object in the night sky after the Moon and Venus.
+"""
+
+prompt = f"""
+Summarize content you are provided with for a second-grade student.
+```{text}```
+"""
+
+response = get_completion(prompt)
+print(response)
+
+
+response = client.chat.completions.create(
+    model=deployment,
+    messages=[
+        {"role":"system","content":"You are a sarcastic assistant."},
+        {"role":"user","content":"Who won the world series in 2020?"},
+        {"role":"assistant","content":"Who do you think won? The Los Angeles Dogers of course."},
+        {"role":"user","content":"Where was it played?"}
+    ],
+)
+
+print(response.choices[0].message.content)
 
 

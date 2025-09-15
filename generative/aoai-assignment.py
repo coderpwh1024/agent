10f8b6115454d261d5a40467ee8a226cf36b54df
pengwenhao <pengwenhao@aliyun.com>
2025-09-15 22:42:20 +0800

    fix:update aoai

--- generative/aoai-assignment.py
+++ generative/aoai-assignment.py
@@ -2,18 +2,7 @@ import os
 
 import tiktoken
 
-# text = f"""
-# Jupiter is the fifth planet from the Sun and the \
-# largest in the Solar System. It is a gas giant with \
-# a mass one-thousandth that of the Sun, but two-and-a-half \
-# times that of all the other planets in the Solar System combined. \
-# Jupiter is one of the brightest objects visible to the naked eye \
-# in the night sky, and has been known to ancient civilizations since \
-# before recorded history. It is named after the Roman god Jupiter.[19] \
-# When viewed from Earth, Jupiter can be bright enough for its reflected \
-# light to cast visible shadows,[20] and is on average the third-brightest \
-# natural object in the night sky after the Moon and Venus.
-# """
+
 
 text=f"""
 木星是太阳系中距离太阳第五近的行星，也是太阳系中体积最大的行星。作为一颗气态巨行星，木星的质量仅为太阳的千分之一，但却是太阳系中所有其他行星质量总和的两倍半。木星是夜空中肉眼可见的最明亮天体之一，在有历史记载之前就已被古代文明所知晓。其名称源于罗马神话中的朱庇特[19]。从地球观测时，木星的亮度足以让其反射的光投射出可见的阴影[20]，是继月球和金星之后，夜空中平均亮度第三的自然天体。
@@ -77,18 +66,7 @@ prompt = f"""
 response = get_completion(prompt)
 print(response)
 
-# text = f"""
-# Jupiter is the fifth planet from the Sun and the \
-# largest in the Solar System. It is a gas giant with \
-# a mass one-thousandth that of the Sun, but two-and-a-half \
-# times that of all the other planets in the Solar System combined. \
-# Jupiter is one of the brightest objects visible to the naked eye \
-# in the night sky, and has been known to ancient civilizations since \
-# before recorded history. It is named after the Roman god Jupiter.[19] \
-# When viewed from Earth, Jupiter can be bright enough for its reflected \
-# light to cast visible shadows,[20] and is on average the third-brightest \
-# natural object in the night sky after the Moon and Venus.
-# """
+
 text=f"""
 木星是太阳系中距离太阳第五近的行星，也是太阳系中最大的行星。它是一颗气态巨行星，质量仅为太阳的千分之一，但却是太阳系中所有其他行星总质量的两倍半。木星是夜空中肉眼可见的最明亮天体之一，在有历史记载之前就已被古代文明所知。它的名字来源于罗马神话中的朱庇特神[19]。从地球观测时，木星的亮度足以让其反射的光投射出可见的阴影[20]，是继月球和金星之后，夜空中平均亮度第三的自然天体。
 """

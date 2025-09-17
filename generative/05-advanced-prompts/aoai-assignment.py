e4681989568c82d11bdd6dda626e2e1b6b710ea8
pengwenhao <pengwenhao@aliyun.com>
2025-09-17 12:50:31 +0800

    feat:init aoai-assignment

---
+++ generative/05-advanced-prompts/aoai-assignment.py
@@ -0,0 +1,11 @@
+from flask import Flask,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    name=request.args.get('name','world')
+    return  f'hello,{name}!'
+
+if __name__=='__main__':
+    app.run(debug=True)

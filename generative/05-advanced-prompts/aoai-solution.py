37c7a5aa9238cf3690409d2c8115a55e16bb8b1d
pengwenhao <pengwenhao@aliyun.com>
2025-09-17 12:48:23 +0800

    fix:update aoai-solution

--- generative/05-advanced-prompts/aoai-solution.py
+++ generative/05-advanced-prompts/aoai-solution.py
@@ -1,9 +1,39 @@
 from flask import Flask, request
-from flask_wtf import  FlaskForm
-from wtforms import StringField,SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'your-secret-key'
+
+
+class HelloForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=3)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    form = HelloForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        return f'Hello,{name}({email})!'
+    return form.render_template()
+
+
+@app.errorhandler(400)
+def bad_request():
+    return 'Bad Request', 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
 

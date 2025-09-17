768e031f298d7d85efd14db7e016ca5db2da8338
pengwenhao <pengwenhao@aliyun.com>
2025-09-17 22:18:33 +0800

    fix:update aoai-app-recipe

--- generative/06-text-generation-apps/aoai-app-recipe.py
+++ generative/06-text-generation-apps/aoai-app-recipe.py
@@ -1,6 +1,6 @@
-from openai import  AzureOpenAI
+from openai import AzureOpenAI
 import os
-from dotenv import  load_dotenv
+from dotenv import load_dotenv
 
 client = AzureOpenAI(
 
@@ -10,11 +10,29 @@ client = AzureOpenAI(
     api_version="2024-08-01-preview",
 )
 
-deployment =""
+deployment = ""
 
 no_recipes = input("No of recipes (for example ,5:")
 
 ingredients = input("List of ingredients (for example, chicken,potatoes,and carrots:")
 
-filter =input("Filter (for exmaple,vegetarian,vegan,or gluten-free:")
+filter = input("Filter (for exmaple,vegetarian,vegan,or gluten-free:")
+
+prompt = f"""Show me {no_recipes} recipes for a dish with the following ingredients:{ingredients} . Per recipe,list all the ingredients used,no {filter}:"""
+
+messages = [{"role": "user", "content": prompt}]
+
+completion = client.chat.completions.create(
+    model=deployment,
+    messages=messages,
+    temperature=0.1,
+    max_tokens=1024
+)
+
+print("Recipes:")
+print(completion.choices[0].message.content)
+
+old_prompt_result= completion.choices[0].message.content
+
+
 

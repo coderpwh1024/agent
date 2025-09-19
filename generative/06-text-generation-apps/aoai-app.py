c1cce551fad3185ed4a287415f3d2cdd744f84e1
pengwenhao <pengwenhao@aliyun.com>
2025-09-19 15:36:13 +0800

    fix:update aoai-app

--- generative/06-text-generation-apps/aoai-app.py
+++ generative/06-text-generation-apps/aoai-app.py
@@ -1,15 +1,11 @@
-from IPython.core.debugger import prompt
-from grpc.framework.interfaces.base.utilities import completion
 from openai import AzureOpenAI
-import os
 from dotenv import load_dotenv
-from torch.ao.quantization import per_channel_weight_observer_range_neg_127_to_127
 
 load_dotenv()
 
-endpoint = ""
-deployment = " "
-apiKey = " "
+endpoint=""
+deployment=" "
+apiKey=" "
 
 client = AzureOpenAI(
     azure_endpoint=endpoint,
@@ -17,9 +13,8 @@ client = AzureOpenAI(
     api_version="2024-08-01-preview",
 )
 
-deployment = ""
 
-prompt = """Complete the following:Once upon a time there was a"""
+prompt="""从前有一个"""
 
 messages = [{"role": "user", "content": prompt}]
 completion = client.chat.completions.create(model=deployment, messages=messages)

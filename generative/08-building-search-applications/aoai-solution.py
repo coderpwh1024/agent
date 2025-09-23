2ac0c07df8e10982891013bfb219fc89a96566bc
pengwenhao <pengwenhao@aliyun.com>
2025-09-23 21:48:27 +0800

    fix:update aoai-solution

--- generative/08-building-search-applications/aoai-solution.py
+++ generative/08-building-search-applications/aoai-solution.py
@@ -21,3 +21,8 @@ client = AzureOpenAI(
 model = 'text-embedding-3-large'
 SIMILARITIES_RESULTS_THRESHOLD = 0.75
 DATASET_NAME = "../embedding_index_3m.json"
+
+
+def load_dataset(source:str)->pd.core.frame.DataFrame:
+    pd_vectors = pd.read_json(source)
+    return  pd_vectors.drop(columns=["text"],errors="ignore").fillna("")

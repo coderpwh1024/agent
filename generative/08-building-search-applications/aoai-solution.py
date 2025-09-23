c07036a309eb45a6969d781b50a93e7c34d5216c
pengwenhao <pengwenhao@aliyun.com>
2025-09-23 21:41:45 +0800

    feat:init aoai-solution

---
+++ generative/08-building-search-applications/aoai-solution.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import numpy as np
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+from qdrant_client.http import model
+
+load_dotenv()
+
+endpoint = " "
+deployment = " "
+apiKey = " "
+
+client = AzureOpenAI(
+    azure_endpoint=endpoint,
+    api_key=apiKey,
+    api_version="2024-08-01-preview"
+)
+
+# embedding 模型
+model = 'text-embedding-3-large'
+SIMILARITIES_RESULTS_THRESHOLD = 0.75
+DATASET_NAME = "../embedding_index_3m.json"

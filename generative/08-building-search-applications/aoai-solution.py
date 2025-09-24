cad6460e3c08fdfed09dcc41dd861673608ce9bc
pengwenhao <pengwenhao@aliyun.com>
2025-09-24 11:38:53 +0800

    fix:update cosine_similarity

--- generative/08-building-search-applications/aoai-solution.py
+++ generative/08-building-search-applications/aoai-solution.py
@@ -23,6 +23,17 @@ SIMILARITIES_RESULTS_THRESHOLD = 0.75
 DATASET_NAME = "../embedding_index_3m.json"
 
 
+
+# 加载JSON 数据
 def load_dataset(source:str)->pd.core.frame.DataFrame:
     pd_vectors = pd.read_json(source)
     return  pd_vectors.drop(columns=["text"],errors="ignore").fillna("")
+
+
+# 余弦相似度
+def cosine_similarity(a,b):
+    if len(a)>len(b):
+        b=np.pad(b,(0,len(a)-len(b)),'constant')
+    elif len(b)>len(a):
+        a=np.pad(a,(0,len(b)-len(a)),'constant')
+    return np.dot(a,b)/(np.linalg.norm(a)*np.linalg.norm(b))

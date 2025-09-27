5cb2181f7a8eed06067c64429b94dd486512eb70
pengwenhao <pengwenhao@aliyun.com>
2025-09-27 14:58:42 +0800

    fix:update aoai-assignment

--- generative/11-integrating-with-function-calling/aoai-assignment.py
+++ generative/11-integrating-with-function-calling/aoai-assignment.py
@@ -39,11 +39,9 @@ import json
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 
-
-endpoint=" "
-deployment=" "
-apiKey=" "
-
+endpoint = " "
+deployment = " "
+apiKey = " "
 
 client = AzureOpenAI(
     azure_endpoint=endpoint,
@@ -75,3 +73,44 @@ print("======================================================")
 # print(json_response1)
 # print("\n")
 # print(json_response2)
+
+
+messages = [{"role": "user", "content": "Find me a good course for a beginner student to learn Azure ."}]
+
+functions=[
+    {
+        "name":"search_courses",
+        "description":"Retrieves course from the search index based on the parameters provided",
+        "parameters":{
+            "role":{
+                "type":"string",
+                "description":"The role of the learner (i.e. developer, data scientist, student, etc.)"
+            },
+            "product":{
+                "type":"string",
+                "description":"The product that the lesson is covering (i.e. Azure, Power BI, etc.)"
+            },
+            "level":{
+                "type":"string",
+                "description":"The level of experience the learner has prior to taking the course (i.e. beginner, intermediate, advanced)"
+
+            }
+        },
+        "required":["role"]
+    }
+]
+
+reponse = client.chat.completions.create(
+    model=deployment,
+    messages=messages,
+    functions=functions,
+    function_call="auto"
+)
+print(reponse.choices[0].message)
+
+
+
+
+
+
+

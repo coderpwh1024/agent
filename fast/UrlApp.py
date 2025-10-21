4ce583a167fff6a9a70b49bd454d6077d5b997ff
coderpwh1024 <pengwenhao@aliyun.com>
2025-10-21 15:36:34 +0800

    fix:update UrlApp

--- fast/UrlApp.py
+++ fast/UrlApp.py
@@ -28,15 +28,13 @@ class ModelName(str, Enum):
 
 @app.get("/models/{model_name}")
 async  def get_model(model_name: ModelName):
-    if model_name is ModelName.lenet:
+    if model_name is ModelName.alexnet:
         return {"model_name":model_name,"message":"Deep Learning FTW"}
 
     if model_name.value== "lenet":
         return {"model_name":model_name,"message":"LeCNN all the images"}
 
     return {"model_name":model_name,"message":"Have some residuals"}
-
-
 
 
 if __name__ == "__main__":

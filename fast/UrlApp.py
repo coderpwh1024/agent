dcf97ce5cbc7b52350e6bcf606a4938c9b362bd7
coderpwh <coderpwh@gmail.com>
2025-10-21 14:18:32 +0800

    fix:update urlApp

---
+++ fast/UrlApp.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import  FastAPI
+
+app = FastAPI()
+
+# python 中关于url路径参数
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    print("item_id:",item_id)
+    return {"item_id":item_id}
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)

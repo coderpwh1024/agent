88b41d527c61a55b51b775da018c0a9dcff110b9
coderpwh1024 <pengwenhao@aliyun.com>
2025-10-21 18:00:18 +0800

    fix:udpate RequestBody

--- fast/RequestBodyApp.py
+++ fast/RequestBodyApp.py
@@ -12,10 +12,25 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+# 必选参数查询
 @app.post("/items/")
 async def create_item(item: Item):
     print("item:", item)
     return "success"
+
+
+# 编辑与新增参数等
+@app.post("/items2/")
+async def create_item2(item: Item):
+    print("item:", item)
+    item_dict = item.dict()
+    if item_dict is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+
 
 
 if __name__ == "__main__":

b0e8332bd69eda0fc93254cefe1cccf28fd268fa
coderpwh1024 <pengwenhao@aliyun.com>
2025-10-22 14:54:10 +0800

    fix:update stem5_query_validation

--- fast/step5_query_validation.py
+++ fast/step5_query_validation.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 
 import uvicorn
 from fastapi import FastAPI, Query
@@ -54,6 +54,40 @@ async def read_item6(q: Union[str, None] = Query(min_length=3)):
         results.update({"q": q})
     return results
 
+
+@app.get("/items7/")
+async def read_item7(q: Union[List[str], None] = Query(default=None)):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/items8/")
+async def read_item8(q: Union[List[str], None] = Query(default=["foo", "bar"])):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/items9/")
+async def read_item9(q: list = Query(default=[])):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/items10/")
+async def read_item10(q: Union[str, None] = Query(default=None, title="Query string", min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items11/")
+async def read_item11(q: Union[str, None] = Query(default=None, title="Query string", min_length=3,
+                                                  description="Query string for the items to search in the database that have a good match")):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 

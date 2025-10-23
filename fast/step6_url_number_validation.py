aab89ea623929645082db1b785745c0b5e2e1c4e
coderpwh1024 <pengwenhao@aliyun.com>
2025-10-23 09:48:34 +0800

    fix:update step6_url_number_validation

--- fast/step6_url_number_validation.py
+++ fast/step6_url_number_validation.py
@@ -1,3 +1,5 @@
+from importlib.resources import read_text
+from string import ascii_uppercase
 from typing import Annotated
 import uvicorn
 from fastapi import FastAPI, Path, Query
@@ -17,13 +19,42 @@ async def read_items(
     return results
 
 
-
 @app.get("/items2/{item_id}")
-async  def read_items2(q:str,item_id:int =Path(title="The ID of the item to get")):
+async def read_items2(q: str, item_id: int = Path(title="The ID of the item to get")):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
-    return  results
+    return results
+
+
+@app.get("/items3/{item_id}")
+async def read_items3(*, q: str, item_id: int = Path(title="The ID of the item to get")):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items4/{item_id}")
+async def read_items4(*, item_id: int = Path(title="The ID of the item to get", ge=10, le=200), q: str):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items5/{item_id}")
+async def read_item5(*, item_id: int = Path(title="The ID of the item to get", ge=10, le=100), q: str,
+                     size: float = Query(gt=0, lt=10.5)):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if size:
+        results.update({"size": size})
+    return results
+
+
+
 
 
 if __name__ == "__main__":

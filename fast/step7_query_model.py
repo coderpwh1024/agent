028172065e6027364cf074566f601b9d9c7780f2
coderpwh1024 <pengwenhao@aliyun.com>
2025-10-24 09:35:24 +0800

    fix:update step7

--- fast/step7_query_model.py
+++ fast/step7_query_model.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-
+# 参数模型
 class FilterParams(BaseModel):
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
@@ -14,9 +14,26 @@ class FilterParams(BaseModel):
     tags: list[str] = []
 
 
+# 参数模型，过滤不必要参数
+class FilterParams2(BaseModel):
+    model_config ={"extra":"forbid"}
+    limit: int = Field(100, gt=0, le=100)
+    offset: int=Field(0,ge=0)
+    order_by: Literal["created_at", "update_at"] = "create_at"
+    tags: list[str] = []
+
+
+
+
 @app.get("/items")
 async def read_items(filter_query: Annotated[FilterParams, Query()]):
     return filter_query
+
+
+@app.get("/items2")
+async  def read_items2(filter_query: Annotated[FilterParams2, Query()]):
+    return  filter_query
+
 
 
 if __name__ == "__main__":

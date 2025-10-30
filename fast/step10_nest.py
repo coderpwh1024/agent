58c6a72ee5d62ef46486a90c9efd4a6e3c487219
coderpwh1024 <pengwenhao@aliyun.com>
2025-10-30 17:38:29 +0800

    fix:update step10_netst

--- fast/step10_nest.py
+++ fast/step10_nest.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -12,14 +14,25 @@ class Item(BaseModel):
     tax: list = []
 
 
-
-
+class Item2(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: list[str] = []
 
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+@app.put("/items2/{item_id}")
+async  def update_item2(item_id:int,item:Item2):
+    results = {"item_id": item_id, "item": item}
+    return results
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)

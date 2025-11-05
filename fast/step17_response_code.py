bb1b97d64f06ba7bc88fdda52cfa13807f5e1024
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-05 19:19:57 +0800

    fix:update step17_reponse_code

--- fast/step17_response_code.py
+++ fast/step17_response_code.py
@@ -1,11 +1,16 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,status
 app = FastAPI()
 
 
 
 @app.post("/items/",status_code=201)
 async def create_item(name:str):
+    return  {"name":name}
+
+
+@app.post("/items2/",status_code=status.HTTP_201_CREATED)
+async  def create_item2(name:str):
     return  {"name":name}
 
 

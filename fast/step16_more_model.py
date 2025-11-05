f0356ade2627e4bf40d938de16e33943b0149258
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-05 19:06:58 +0800

    fix:update step16_more_model

--- fast/step16_more_model.py
+++ fast/step16_more_model.py
@@ -93,6 +93,10 @@ async  def read_items2():
     return  items2
 
 
+@app.get("/keyword-weights/", response_model=dict[str,float])
+async  def read_keyword_weights():
+    return {"foo":2.3,"bar":3.4}
+
 
 
 

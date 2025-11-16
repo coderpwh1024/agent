9ab30addf271aa392f1350da9dc35beda2e44169
pengwenhao <pengwenhao@aliyun.com>
2025-11-16 21:38:10 +0800

    fix:update step19_File

--- fast/step19_File.py
+++ fast/step19_File.py
@@ -1,7 +1,14 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 
 app = FastAPI();
+
+
+
+@app.post("/files/")
+async  def create_upload_file(file:bytes=File()):
+    return {"file_size":len(file)}
+
 
 
 

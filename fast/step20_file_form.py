e66091051fb050f714bd3cc8f3723e4810d75f89
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-18 09:21:20 +0800

    fix:update step20_file_form

---
+++ fast/step20_file_form.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, File, Form
+from starlette.datastructures import UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File(), fileb: UploadFile = File(), token: str = Form()):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+

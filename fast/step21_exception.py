e8f5c866d0f0dd5ed7bed5f28d7c807141ea9ce7
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-18 10:56:57 +0800

    fix:update setp21_exception

--- fast/step21_exception.py
+++ fast/step21_exception.py
@@ -1,5 +1,12 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from starlette.responses import JSONResponse
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
 
 app = FastAPI();
 
@@ -11,6 +18,29 @@ async def read_item(item_id: str):
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
     return {"item": items[item_id]}
+
+
+@app.get("/items-header/{item_id}")
+async def read_item_header(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found", headers={"X-Error": "There goes my error"})
+    return {"item": items[item_id]}
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(status_code=418, content={"message": f"{exc.name} is not a unicorn!"})
+
+
+@app.get("/unicorns/{name}")
+async  def read_unicorn(name:str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return  {"unicor_name":name}
+
+
+
+
 
 
 if __name__ == "__main__":

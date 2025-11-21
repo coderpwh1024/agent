5ff4cd097a327aab2a68ef2cbdca1e918f98c5d0
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-21 15:44:25 +0800

    fix:update step22_status_code

--- fast/step22_status_code.py
+++ fast/step22_status_code.py
@@ -1,23 +1,39 @@
-from typing import Union
+from typing import Union, Set
 import uvicorn
-from fastapi import FastAPI,status
+from fastapi import FastAPI, status
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
-    name:str
-    description:Union[str,None]=None
-    price:float
-    tax:Union[float,None]=None
-    tags:list[str]=set()
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    # tags:list[str]=set()
+    tags: Set[str] = set()
 
 
-
-
-@app.post("/items",response_model= Item,status_code=status.HTTP_201_CREATED)
-async  def create_item(item:Item):
+@app.post("/items", response_model=Item, status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
     return item
+
+
+@app.post("/items2", response_model=Item, tags=["items"])
+async def create_item2(item: Item):
+    return item
+
+
+@app.get("/item3", tags=["items"])
+async  def read_items():
+    return {"name": "Foo", "price": 42}
+
+
+
+@app.get("/users", tags=["users"])
+async  def read_users():
+    return [{"username":"johndoe"}]
 
 
 

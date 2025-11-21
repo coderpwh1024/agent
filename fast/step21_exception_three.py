97d0fefd1060bd8c3b57acee8b110dfe7315ec4c
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-21 14:55:58 +0800

    fix:update step21

--- fast/step21_exception_three.py
+++ fast/step21_exception_three.py
@@ -4,6 +4,13 @@ from fastapi import FastAPI
 app = FastAPI();
 
 
+async  def custom_http_exception_handler(request,exc):
+    print(f"OMG! http 请求错误:{repr(exc)}")
+    return  await custom_http_exception_handler(request,exc)
+
+
+
+
 
 if __name__ =="__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)

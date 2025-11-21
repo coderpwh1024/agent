6e463de7f106e23cf9b7ab01bbf6aefadc77cc86
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-21 17:42:02 +0800

    fix:udpate step22_status_code_two

--- fast/step22_stauts_code_two.py
+++ fast/step22_stauts_code_two.py
@@ -15,11 +15,23 @@ class Item(BaseModel):
     tags: Set[str] = set()
 
 
-
-@app.post("/items/",response_model=Item,summary="Create an item",description="Create an item with all the information, name, description, price, tax and a set of unique tags")
-async  def create_item(item: Item):
+@app.post("/items/", response_model=Item, summary="Create an item",
+          description="Create an item with all the information, name, description, price, tax and a set of unique tags")
+async def create_item(item: Item):
     return item
 
+
+@app.post("/items2", response_model=Item, summary="Create an items")
+async def create_item2(item: Item):
+    """
+    Create an item with all the information:
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+    return item
 
 
 

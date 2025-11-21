446d9d50c7a6dde6d27bc36bb3a54aee5fdb6446
coderpwh1024 <pengwenhao@aliyun.com>
2025-11-21 14:58:38 +0800

    fix:update step21_exception_three

--- fast/step21_exception_three.py
+++ fast/step21_exception_three.py
@@ -1,12 +1,21 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from fastapi.exception_handlers import(
+    http_exception_handler,
+request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI();
 
 
+@app.exception_handler(StarletteHTTPException)
 async  def custom_http_exception_handler(request,exc):
     print(f"OMG! http 请求错误:{repr(exc)}")
     return  await custom_http_exception_handler(request,exc)
+
+
 
 
 

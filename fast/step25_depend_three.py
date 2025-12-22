802b7793fe6b5b677178cdae2242e14f2f011ab8
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-22 16:54:05 +0800

    fix:update step25_depend

--- fast/step25_depend_three.py
+++ fast/step25_depend_three.py
@@ -14,7 +14,7 @@ async def verify_token(x_token: str = Header()):
 async def verify_key(x_key: str = Header()):
     if x_key != "fake-super-secret-key":
         raise HTTPException(status_code=400, detail="X-Key header invalid")
-    return {"x_key": x_key}
+    return x_key
 
 @app.get("/items/")
 async  def read_items():

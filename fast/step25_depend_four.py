63afcfe19581410a5a46572a139b2c02867cff6a
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-22 20:13:39 +0800

    feat:init

---
+++ fast/step25_depend_four.py
@@ -0,0 +1,34 @@
+from email.header import Header
+from typing import Annotated
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Depends
+
+
+async def verify_token(x_token: Annotated[str, Header()]):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: Annotated[str, Header()]):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items/")
+async def read_items():
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)

0f13394d1a749269334c696cf4778b3325e090a8
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-22 17:08:01 +0800

    fix:update step25_depend_thtree

--- fast/step25_depend_three.py
+++ fast/step25_depend_three.py
@@ -1,24 +1,24 @@
-from email.header import Header
-
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 app = FastAPI()
 
-
-async def verify_token(x_token: str = Header()):
+async def verify_token(x_token: Annotated[str, Header()]):
     if x_token != "fake-super-secret-token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-async def verify_key(x_key: str = Header()):
+async def verify_key(x_key: Annotated[str, Header()]):
     if x_key != "fake-super-secret-key":
         raise HTTPException(status_code=400, detail="X-Key header invalid")
     return x_key
 
-@app.get("/items/")
-async  def read_items():
-   return [{"item":"Foo"},{"item":"Bar"}]
+
+
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)

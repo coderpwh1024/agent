cc46b389a4cd19da9e88a9f16c0de171c946950b
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-26 10:27:06 +0800

    fix:update step26_depend_one

--- fast/step26_depend_one.py
+++ fast/step26_depend_one.py
@@ -1,10 +1,25 @@
+from http.client import HTTPException
+
 import uvicorn
 from fastapi import FastAPI
 
-app =FastAPI()
+app = FastAPI()
+
+data = {
+    "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
+    "portal-gun": {""}
+}
 
 
+class OwnerError(Exception):
+    pass
 
+
+def get_username():
+    try:
+        yield "Rick"
+    except OwnerError as e:
+        raise HTTPException(status_code=400, detail=f"Owner error:{e}")
 
 
 if __name__ == "__main__":

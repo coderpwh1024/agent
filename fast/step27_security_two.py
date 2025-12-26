4e6875cb4a863dde9c88b2667c018c7a30b0a218
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-26 14:16:18 +0800

    fix:update step27_security_two

--- fast/step27_security_two.py
+++ fast/step27_security_two.py
@@ -30,6 +30,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 
 @app.get("/users/me")
 async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    print("current_user:{}",current_user)
     return current_user
 
 

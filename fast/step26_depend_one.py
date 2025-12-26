1ca75aaaa17903d30115bd7de1236d360538dd05
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-26 10:57:37 +0800

    feat:init step26_depend_one

--- fast/step26_depend_one.py
+++ fast/step26_depend_one.py
@@ -29,6 +29,7 @@ def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
     if item_id not in data:
         raise HTTPException(status_code=404, detail="Item not found")
     item = data[item_id]
+    print("item:{}",item)
     if item["owner"] != username:
         raise OwnerError(username)
     return item

508d19eae6f7ea20b2c8fc7227c9bff7db6a5162
coderpwh1024 <pengwenhao@aliyun.com>
2025-12-26 11:10:20 +0800

    feat:init step26_depend_two

---
+++ fast/step26_depend_two.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException
+
+app = FastAPI()
+
+
+class InternalError(Exception):
+    pass
+
+
+def get_username():
+    try:
+        yield "Rick"
+    except InternalError:
+        print("We don't swallow the internal error here, we raise again 😎")
+        raise
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: str, username: Annotated[str, Depends(get_username())]):
+    if item_id == "portal-gun":
+        raise InternalError(f"The portal gun is too dangerous to be owned by {username}")
+    if item_id != "plumbus":
+        raise HTTPException(status_code=404, detail="Item not found, there's only a plumbus here")
+    return item_id
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
